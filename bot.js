10dc0aae3070d4500fd53500b0f93670def78b9d
ツThe Watchers Bot Devsツ <47341267+GrimDesignsFiveM@users.noreply.github.com>
2019-10-24 22:08:59 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -19,7 +19,7 @@ bot.music.start(bot, {
 
 // Events.
 bot.on("ready", function() {
-    bot.user.setActivity(`Hi, Im The Music Bot`);
+    bot.user.setGame(`Listening To YouTube `, `https://www.twitch.tv/monstercat`);
     console.log(`${bot.user.username} is Ready!`);
 });
 
