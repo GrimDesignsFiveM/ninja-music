86cb58d37a84109a05f99e1d443aa0a0e1c3eb48
ツThe Watchers Bot Devsツ <47341267+GrimDesignsFiveM@users.noreply.github.com>
2019-10-24 22:22:46 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -22,6 +22,34 @@ bot.on("ready", function() {
     bot.user.setGame(`Listening To YouTube `, `https://www.twitch.tv/monstercat`);
     console.log(`${bot.user.username} is Ready!`);
 });
+
+client.on("message", async message => {
+  // This event will run on every single message received, from any channel or DM.
+  
+  // It's good practice to ignore other bots. This also makes your bot ignore itself
+  // and not get into a spam loop (we call that "botception").
+  if(message.author.bot) return;
+  
+  // Also good practice to ignore any message that does not start with our prefix, 
+  // which is set in the configuration file.
+  if(message.content.indexOf(prefix) !== 0) return;
+  
+  // Here we separate our "command" name, and our "arguments" for the command. 
+  // e.g. if we have the message "+say Is this the real life?" , we'll get the following:
+  // command = say
+  // args = ["Is", "this", "the", "real", "life?"]
+  const args = message.content.slice(prefix.length).trim().split(/ +/g);
+  const command = args.shift().toLowerCase();
+ 
+if (command === "help") {
+let helpBed = new Discord.RichEmbed()
+    .setColor(0x00AE86)
+    .setTitle("Not Yet")
+    .setDescription("My help command is still being developed")
+    message.delete().catch()
+}
+
+ 
  
 // Bot Login.
 // bot.login('YourAwesomeBotToken');
